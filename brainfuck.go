9df87940a0bc7d1eda2567bfbc9a4f71bf14e336
shina <zodiac_oracle@docomo.ne.jp>
2022-10-04 11:39:41 +0900

    Add outline

---
+++ brainfuck.go
@@ -0,0 +1,22 @@
+package main
+
+const (
+	increment = '+'
+	decrement = '-'
+	right     = '>'
+	left      = '<'
+	loopStart = '['
+	loopEnd   = ']'
+	output    = '.'
+	input     = ','
+)
+
+const memorySize = 1024
+
+func main() {
+	var (
+		memory  [memorySize]byte
+		ptr     int
+		codePtr int
+	)
+}
