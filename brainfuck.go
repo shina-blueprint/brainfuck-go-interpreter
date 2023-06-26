aed9e6526dd905edc3a077b8ffb1e74ddfde7d54
shina <shina.blueprint@gmail.com>
2023-06-27 00:20:59 +0900

    Add loading file function

--- brainfuck.go
+++ brainfuck.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 const (
 	increment = '+'
 	decrement = '-'
@@ -19,4 +24,21 @@ func main() {
 		ptr     int
 		codePtr int
 	)
+
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Error: A Brainfuck code is not passed as a command-line argument.")
+		fmt.Fprintln(os.Stderr, "Please pass an argument as the form, $ ./brainfuck [FILENAME].")
+		os.Exit(1)
+	}
+
+	code, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: The file, "+os.Args[1]+", cannot be opened.")
+		os.Exit(1)
+	}
+
+	codeLen := len(code)
+
+	fmt.Println(codeLen, " Byte.")
+	fmt.Println(code)
 }
